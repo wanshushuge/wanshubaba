d86f4d5f6c5d81f8ea5804f90f303ebdc944a553
wanshushuge <719765296@qq.com>
2026-09-08 20:19:26 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,7 +15,6 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE)
       .then((c) => c.addAll(ASSETS))
-      .then(() => self.skipWaiting())
   );
 });
 
@@ -25,6 +24,10 @@ self.addEventListener('activate', (e) => {
       .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
       .then(() => self.clients.claim())
   );
+});
+
+self.addEventListener('message', (e) => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
